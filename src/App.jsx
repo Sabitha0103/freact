840368f7613b040dabd1b4855c3343609f4fdc93
234g1a33d5@srit.ac.in <you@example.com>
2025-09-21 15:50:37 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import StudentForm from "./StudentForm";
 import StudentList from "./StudentList";
 import './assets/css/style.css'; 
 
-const API_URL = "http://localhost:8081/students";
+const API_URL = "https://student-production-49b0.up.railway.app/students";
 
 function App() {
   const [students, setStudents] = useState([]);
